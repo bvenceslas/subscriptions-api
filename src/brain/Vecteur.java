68eff1dbe291df84ea9ffdef7a5fab5df42acdbc
Venceslas Burongu <bvenceslas@gmail.com>
2020-02-14 21:47:42 +0200

    Create Vecteur.java

---
+++ src/brain/Vecteur.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brain;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author Moses Brain
+ */
+public class Vecteur {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+        Vector list = new Vector();
+        int len=args.length;
+        for(int i=0;i<len;i++)
+        {
+                list.addElement(args[i]);
+        }
+        int size=list.size();
+        String str[]= new String[size];
+        list.copyInto(str);
+        for(int i=0;i<size;i++)
+        {
+                System.out.println ("Element of Vector at position "+i+":"+str[i]);
+        }
+    }    
+}
