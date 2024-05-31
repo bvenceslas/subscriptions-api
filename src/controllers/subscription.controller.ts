c7b2ba5afee97490ab980c814fa659a06073d370
bvenceslas <bvenceslas@gmail.com>
2024-05-31 09:09:09 +0200

    test sending message using private prhone number

--- src/controllers/subscription.controller.ts
+++ src/controllers/subscription.controller.ts
@@ -1,32 +1,61 @@
 import { Request, Response } from "express";
 import { User } from "../models/user.model";
-import { sendTelegramMessage } from "../utils/telegramBot";
+import {
+  sendPrivateTelegramMessage,
+  sendTelegramMessage,
+} from "../utils/telegramBot";
 
 export const subscribe = async (req: Request, res: Response) => {
-  const { name, email } = req.body;
+  const { name, phoneNumber, email } = req.body;
+
+  console.log("data", name, email, phoneNumber);
 
   try {
     // check if the user is already subscribed
-    const foundUser = await User.findOne({ email });
+    const foundUserByEmail = await User.findOne({ email });
 
-    if (foundUser) {
-      return res.status(409).json({ message: "Already subscribed!" });
+    console.log("foundUserByEmail", foundUserByEmail);
+
+    if (foundUserByEmail) {
+      return res.status(409).json({ message: "Email already subscribed!" });
     }
 
+    const foundUserByPhoneNumber = await User.findOne({ phoneNumber });
+
+    if (foundUserByPhoneNumber) {
+      return res
+        .status(409)
+        .json({ message: "Phone Number already subscribed!" });
+    }
+
+    console.log("foundUserByPhoneNumber", foundUserByPhoneNumber);
+
     // create and save the new user
-    const newUser = new User({ name, email });
-    console.log("newUser", newUser);
-    const saved = await newUser.save();
+    const newUser = new User({ name, email, phoneNumber });
+    await newUser.save();
 
     // count emails
     const count = await User.countDocuments();
-    const message = `New Subscription ${email} [${name}]
-    Subscription ${count}`;
 
-    // send message
-    await sendTelegramMessage(message);
+    const chanelMessage = `New Subscription 
+    Email: ${email} 
+    Name: ${name}
+    Phone Number: ${phoneNumber}
+
+    Subscription #${count}`;
+
+    // const privateMessage = `Dear ${name},
+    // Thanks for your subscription to Brain-Subscription-Bot,
+    // Let's keep in touch`;
+
+    // get message from the bot
+    await sendTelegramMessage(chanelMessage);
+
+    // send the telegram message to the subscribed user
+    // await sendPrivateTelegramMessage(phoneNumber, privateMessage);
     return res.status(201).json({ message: "Subscribed successfully", count });
   } catch (error: unknown) {
+    console.error(error);
     return res.status(500).json({ message: "Internal server error" });
   }
 };
--- src/utils/telegramBot.ts
+++ src/utils/telegramBot.ts
@@ -8,3 +8,10 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!);
 export const sendTelegramMessage = async (message: string) => {
   await bot.telegram.sendMessage(process.env.TELEGRAM_CHAT_ID!, message);
 };
+
+export const sendPrivateTelegramMessage = async (
+  phoneNumber: string,
+  message: string
+) => {
+  await bot.telegram.sendMessage(phoneNumber, message);
+};
